c314023b8259bd3516826f09738faab6a34416f0
Ilgiz Valeev <skif-vi@yandex.ru>
2024-01-16 21:48:45 +0500

    create func last

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import _ from "lodash";
-console.log("Hello, Hexlet!");
+import last from "./src/index.js";
+/*console.log("Hello, Hexlet!");
 _.intersection([2, 1], [2, 3]);
-_.capitalize("hello");
-console.log(_.last(["one", "two"]));
+_.capitalize("hello");*/
+console.log(last(["one", "two"]));
